4e010599acdcdc3271ff8d7d9fc156808b4bbf6c
Priyank Parashar <paras20xx@gmail.com>
2019-07-05 00:11:31 +0400

    Refactored the approach for running mocha tests

--- test/headless-integration.test.js
+++ test/headless-integration.test.js
@@ -6,10 +6,13 @@ const
     path = require('path'),
     fs = require('fs');
 
-const { expect } = require('chai');
+const
+    { expect } = require('chai'),
+    glob = require('glob'),
+    RJSON = require('relaxed-json');
 
 const
-    RJSON = require('relaxed-json');
+    { asyncRunTest } = require('../src/headless-integration.js');
 
 const requireRelaxedJson = function (rjsonPath) {
     const contents = fs.readFileSync(path.join(__dirname, rjsonPath), 'utf8');
@@ -20,39 +23,23 @@ describe('Application', async function () {
     // https://stackoverflow.com/questions/16607039/in-mocha-testing-while-calling-asynchronous-function-how-to-avoid-the-timeout-er/16607408#16607408
     this.timeout(15000);
 
-    const { asyncRunTest } = require('../src/headless-integration.js');
-    const main = async function (test, dirPath) {
-        const testStatus = await asyncRunTest(test, dirPath);
-        return testStatus;
-    };
+    const files = glob.sync('./**/*.rjson', {cwd: __dirname});
 
-    it('should load the basic example properly', async function () {
-        const test = requireRelaxedJson('./basic-test/basic-test.rjson');
-        const testStatus = await main(test, path.join(__dirname, 'basic-test'));
-        expect(testStatus).to.equal(true);
-    });
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const test = requireRelaxedJson(file);
+        const testSummary = test.summary || file;
 
-    it('should be able to read innerText for a given selector', async function () {
-        const test = requireRelaxedJson('./test-innerText-at-npmjs-com/test-innerText-at-npmjs-com.rjson');
-        const testStatus = await main(test, path.join(__dirname, 'test-innerText-at-npmjs-com'));
-        expect(testStatus).to.equal(true);
-    });
-
-    it('should be able to take screenshot for a given selector', async function () {
-        const test = requireRelaxedJson('./test-readme-tab-at-npmjs-com/test-readme-tab-at-npmjs-com.rjson');
-        const testStatus = await main(test, path.join(__dirname, 'test-readme-tab-at-npmjs-com'));
-        expect(testStatus).to.equal(true);
-    });
-
-    it('should be able to perform a simple click action', async function () {
-        const test = requireRelaxedJson('./test-simple-click-action/test-simple-click-action.rjson');
-        const testStatus = await main(test, path.join(__dirname, 'test-simple-click-action'));
-        expect(testStatus).to.equal(true);
-    });
-
-    it('should be able to type in an input field', async function () {
-        const test = requireRelaxedJson('./test-typing/test-typing.rjson');
-        const testStatus = await main(test, path.join(__dirname, 'test-typing'));
-        expect(testStatus).to.equal(true);
-    });
+        let itFn = it;
+        if (test.skip) {
+            itFn = it.skip;
+        } else if (test.only) {
+            itFn = it.only;
+        }
+        itFn(testSummary, async function () {
+            const test = requireRelaxedJson(file);
+            const testStatus = await asyncRunTest(test, path.dirname(path.join(__dirname, file)));
+            expect(testStatus).to.equal(true);
+        });
+    }
 });
