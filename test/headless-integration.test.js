03099127e655faef3097d1e435401d2b7d6f8b13
Priyank Parashar <paras20xx@gmail.com>
2019-07-04 23:30:37 +0400

    Now using relaxed-json for the tests

--- test/headless-integration.test.js
+++ test/headless-integration.test.js
@@ -2,9 +2,19 @@
 
 /* globals describe, it */
 
-const path = require('path');
+const
+    path = require('path'),
+    fs = require('fs');
 
 const { expect } = require('chai');
+
+const
+    RJSON = require('relaxed-json');
+
+const requireRelaxedJson = function (rjsonPath) {
+    const contents = fs.readFileSync(path.join(__dirname, rjsonPath), 'utf8');
+    return RJSON.parse(contents);
+};
 
 describe('Application', async function () {
     // https://stackoverflow.com/questions/16607039/in-mocha-testing-while-calling-asynchronous-function-how-to-avoid-the-timeout-er/16607408#16607408
@@ -17,31 +27,31 @@ describe('Application', async function () {
     };
 
     it('should load the basic example properly', async function () {
-        const test = require('./basic-test/basic-test.js');
+        const test = requireRelaxedJson('./basic-test/basic-test.rjson');
         const testStatus = await main(test, path.join(__dirname, 'basic-test'));
         expect(testStatus).to.equal(true);
     });
 
     it('should be able to read innerText for a given selector', async function () {
-        const test = require('./test-innerText-at-npmjs-com/test-innerText-at-npmjs-com.js');
+        const test = requireRelaxedJson('./test-innerText-at-npmjs-com/test-innerText-at-npmjs-com.rjson');
         const testStatus = await main(test, path.join(__dirname, 'test-innerText-at-npmjs-com'));
         expect(testStatus).to.equal(true);
     });
 
     it('should be able to take screenshot for a given selector', async function () {
-        const test = require('./test-readme-tab-at-npmjs-com/test-readme-tab-at-npmjs-com.js');
+        const test = requireRelaxedJson('./test-readme-tab-at-npmjs-com/test-readme-tab-at-npmjs-com.rjson');
         const testStatus = await main(test, path.join(__dirname, 'test-readme-tab-at-npmjs-com'));
         expect(testStatus).to.equal(true);
     });
 
     it('should be able to perform a simple click action', async function () {
-        const test = require('./test-simple-click-action/test-simple-click-action.js');
+        const test = requireRelaxedJson('./test-simple-click-action/test-simple-click-action.rjson');
         const testStatus = await main(test, path.join(__dirname, 'test-simple-click-action'));
         expect(testStatus).to.equal(true);
     });
 
     it('should be able to type in an input field', async function () {
-        const test = require('./test-typing/test-typing.js');
+        const test = requireRelaxedJson('./test-typing/test-typing.rjson');
         const testStatus = await main(test, path.join(__dirname, 'test-typing'));
         expect(testStatus).to.equal(true);
     });
