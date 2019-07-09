00459cb082aad051856e30a5eb8b4efe35691771
Priyank Parashar <paras20xx@gmail.com>
2019-07-10 03:40:33 +0400

    Added support for loading variables into test

--- test/headless-integration.test.js
+++ test/headless-integration.test.js
@@ -15,7 +15,7 @@ const
     { asyncRunTest } = require('../src/headless-integration.js');
 
 const requireRelaxedJson = function (rjsonPath) {
-    const contents = fs.readFileSync(path.join(__dirname, rjsonPath), 'utf8');
+    const contents = fs.readFileSync(path.resolve(__dirname, rjsonPath), 'utf8');
     return RJSON.parse(contents);
 };
 
@@ -27,7 +27,7 @@ describe('Application', async function () {
 
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
-        const test = requireRelaxedJson(file);
+        let test = requireRelaxedJson(file);
         const testSummary = test.summary || file;
 
         let itFn = it;
@@ -37,7 +37,39 @@ describe('Application', async function () {
             itFn = it.only;
         }
         itFn(testSummary, async function () {
-            const test = requireRelaxedJson(file);
+            // ! IMPORTANT: This variable is used by the "eval" statement
+            let _$variables; // eslint-disable-line no-unused-vars
+
+            if (typeof test._$variables === 'undefined') {
+                // do nothing
+            } else if (test._$variables) {
+                if (typeof test._$variables === 'string') {
+                    _$variables = requireRelaxedJson(path.resolve(__dirname, path.dirname(file), test._$variables));
+                } else if (Array.isArray(test._$variables)) {
+                    console.log('Invalid value for _$variables for test in file: ' + file);
+                    console.log('Exiting with error.');
+                    process.exit(1);
+                } else if (typeof test._$variables === 'object') {
+                    _$variables = test._$variables;
+                }
+
+                const stringifiedTest = JSON.stringify(test);
+
+                // ! IMPORTANT: "Double" stringifying to handle escaping properly
+                let doubleStringifiedTest = JSON.stringify(stringifiedTest);
+                doubleStringifiedTest = doubleStringifiedTest.substring(1, doubleStringifiedTest.length - 1);
+
+                const
+                    stringifiedTestWrappedInTemplateLiteral = '`' + doubleStringifiedTest + '`',
+                    stringifiedTestWithVariablesFilledUp = eval(stringifiedTestWrappedInTemplateLiteral);
+
+                test = (JSON.parse(stringifiedTestWithVariablesFilledUp));
+            } else {
+                console.log('Invalid value for _$variables for test in file: ' + file);
+                console.log('Exiting with error.');
+                process.exit(1);
+            }
+
             const testStatus = await asyncRunTest(test, path.dirname(path.join(__dirname, file)));
             expect(testStatus).to.equal(true);
         });
