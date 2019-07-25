175f64315bf8a5420b342912a8015e05fdf65f29
Priyank Parashar <paras20xx@gmail.com>
2019-07-25 19:37:51 +0400

    wip

--- server/http-pub/index.js
+++ server/http-pub/index.js
@@ -9,3 +9,16 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
+
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('temp'));
+}
+
+setInterval(tick, 1000);
+tick();
